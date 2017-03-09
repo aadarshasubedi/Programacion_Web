7955e36c68605013ad5bc13792cd5cba9d8002dd
Osvaldo <osv_93@hotmail.es>
2017-03-08 22:11:37 -0600

    Agregar Script Osvaldo
    Agregar Script Osvaldo

--- BD_Elearning.sql
+++ BD_Elearning.sql
@@ -103,6 +103,80 @@ CREATE TABLE IF NOT EXISTS `tb_Usuario` (
   PRIMARY KEY (`Id_Usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_Curso`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_Curso`
+(
+`Id_Curso` int NOT NULL AUTO_INCREMENT,
+`Nombre` varchar(45) NOT NULL,
+`Duracion` int NOT NULL,
+`Fecha_Inicio` Date NOT NULL,
+`Fecha_Final` Date NOT NULL,
+CONSTRAINT pk_Curso PRIMARY KEY (Id_Curso)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_Tarea`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_Tarea`
+(
+`Id_Tarea` int NOT NULL AUTO_INCREMENT,
+`Id_Curso` int NOT NULL,
+`Descripcion` varchar(300) NOT NULL,
+`Calificacion` int NOT NULL,
+`Fecha_Creacion` Date NOT NULL,
+`Fecha_Entrega` Date NOT NULL,
+`Puntaje` int NOT NULL,
+CONSTRAINT pk_Tarea PRIMARY KEY (Id_Tarea),
+CONSTRAINT fk_Tarea_Curso FOREIGN KEY (Id_Curso) REFERENCES tb_Curso (Id_Curso)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_Semana`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_Semana`
+(
+`Id_Semana` int NOT NULL AUTO_INCREMENT,
+`Id_Curso` int NOT NULL,
+`Fecha_Inicio` Date NOT NULL,
+`Fecha_Final` Date NOT NULL,
+CONSTRAINT pk_Semana PRIMARY KEY (Id_Semana),
+CONSTRAINT fk_Semana_Curso FOREIGN KEY (Id_Curso) REFERENCES tb_Curso (Id_Curso)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_Recurso`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_Recurso`
+(
+`Id_Recurso` int NOT NULL AUTO_INCREMENT,
+`Id_Tipo_Recurso` int NOT NULL,
+`Recurso_Padre` int NOT NULL,
+`Nombre` varchar(30) NOT NULL,
+`Url` varchar(50) NOT NULL,
+`Visible` bit NOT NULL,
+`Secuencia` int NOT NULL,
+`Notas` float NOT NULL,
+`Estado` bit NOT NULL,
+`Semana` int NOT NULL,
+CONSTRAINT pk_Recurso PRIMARY KEY (Id_Recurso),
+CONSTRAINT fk_Recurso_Tipo_Recurso FOREIGN KEY (Id_Tipo_Recurso) REFERENCES tb_Tipo_Recurso (Id_Tipo_Recurso),
+CONSTRAINT fk_Recurso_Padre FOREIGN KEY (Recurso_Padre) REFERENCES tb_Recurso (Id_Recurso)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

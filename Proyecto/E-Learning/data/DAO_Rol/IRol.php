f8d120cc94b693e983d9ff4c742149f42acbe48b
jean saborio <jecasau14@gmail.com>
2017-04-02 22:20:37 -0600

    Rol
    Rol

---
+++ Proyecto/E-Learning/data/DAO_Rol/IRol.php
@@ -0,0 +1,12 @@
+<?php
+
+interface IRol {
+    public function agregar($rol);
+    public function eliminar($Id_Rol);
+    /*public function eliminar($Id_Rol);*/
+    public function modificar($rol);
+    public function consultar($Id_Rol);
+    public function listar();
+}
+
+?>

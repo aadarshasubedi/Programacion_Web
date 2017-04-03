8a18fdee0fd4d23bed6a6ee80c119acf7499707f
Osvaldo <osv_93@hotmail.es>
2017-04-02 19:10:58 -0600

    Revert "cambios"
    This reverts commit 234a83f9e0ce6dd16e3d6d839e5c621218455e8b.

--- Proyecto/Scripts/procedures.sql
+++ Proyecto/Scripts/procedures.sql
@@ -1,38 +1,3 @@
---Agregar Cursos
-INSERT INTO `bd_elearning`.`tb_curso` (`Nombre`, `Duracion`, `Fecha_Inicio`, `Fecha_Final`, `Estado`) 
-VALUES ('Español', '0', '2017-02-06', '2017-06-01', 1);
-
-INSERT INTO `bd_elearning`.`tb_curso` (`Nombre`, `Duracion`, `Fecha_Inicio`, `Fecha_Final`, `Estado`) 
-VALUES ('Inglés', '0', '2017-02-06', '2017-06-01', 1);
-
---Agregar Genero
-INSERT INTO `bd_elearning`.`tb_genero` (`Id_Genero`, `Descripcion`) VALUES ('1', 'Masculino');
-INSERT INTO `bd_elearning`.`tb_genero` (`Id_Genero`, `Descripcion`) VALUES ('2', 'Femenino');
-
---Agregar Roles
-INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
-VALUES ('1', 'Administrador', 1);
-
-INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
-VALUES ('2', 'Editor', 1);
-
-INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
-VALUES ('3', 'Moderador', 1);
-
-INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
-VALUES ('4', 'Profesor', 1);
-
-INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
-VALUES ('5', 'Estudiante', 1);
-
---Agregar Usuarios
-INSERT INTO `BD_Elearning`.`tb_Usuario` (`Id_Usuario`, `Nombre`, `Primer_Apellido`, `Segundo_Apellido`, `Clave`, `Id_Genero`, `Pais`, `Fecha_Ultimo_Ingreso`, `IP`, `SO`, `Navegador`, `Lenguaje`) 
-VALUES ('1', 'Osvaldo', 'Aguero', 'Perez', '1', '1', 'Costa Rica', '2017-01-01', '1.1.1.1', 'Windows', 'Explorer', 'L1');
-
---Asociar Usuarios con Roles
-INSERT INTO `bd_elearning`.`tb_usuario_rol` (`Id_Usuario_Rol`, `Id_Usuario`, `Id_Rol`, `Estado`) 
-VALUES ('1', '1', '1',  1);
-
 DROP PROCEDURE IF EXISTS pr_loginUsuario;
 DELIMITER $$
 CREATE PROCEDURE pr_loginUsuario
@@ -121,6 +86,42 @@ CREATE PROCEDURE pr_actualizarUsuario
 	COMMIT;
  END $$
 DELIMITER ;
+
+
+--Agregar Cursos
+INSERT INTO `bd_elearning`.`tb_curso` (`Nombre`, `Duracion`, `Fecha_Inicio`, `Fecha_Final`, `Estado`) 
+VALUES ('Español', '0', '2017-02-06', '2017-06-01', 1);
+
+INSERT INTO `bd_elearning`.`tb_curso` (`Nombre`, `Duracion`, `Fecha_Inicio`, `Fecha_Final`, `Estado`) 
+VALUES ('Inglés', '0', '2017-02-06', '2017-06-01', 1);
+
+--Agregar Genero
+INSERT INTO `bd_elearning`.`tb_genero` (`Id_Genero`, `Descripcion`) VALUES ('1', 'Masculino');
+INSERT INTO `bd_elearning`.`tb_genero` (`Id_Genero`, `Descripcion`) VALUES ('2', 'Femenino');
+
+--Agregar Roles
+INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
+VALUES ('1', 'Administrador', 1);
+
+INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
+VALUES ('2', 'Editor', 1);
+
+INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
+VALUES ('3', 'Moderador', 1);
+
+INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
+VALUES ('4', 'Profesor', 1);
+
+INSERT INTO `bd_elearning`.`tb_rol` (`Id_Rol`, `Nombre`, `Estado`) 
+VALUES ('5', 'Estudiante', 1);
+
+--Agregar Usuarios
+INSERT INTO `BD_Elearning`.`tb_Usuario` (`Id_Usuario`, `Nombre`, `Primer_Apellido`, `Segundo_Apellido`, `Clave`, `Id_Genero`, `Pais`, `Fecha_Ultimo_Ingreso`, `IP`, `SO`, `Navegador`, `Lenguaje`) 
+VALUES ('1', 'Osvaldo', 'Aguero', 'Perez', '1', '1', 'Costa Rica', '2017-01-01', '1.1.1.1', 'Windows', 'Explorer', 'L1');
+
+--Asociar Usuarios con Roles
+INSERT INTO `bd_elearning`.`tb_usuario_rol` (`Id_Usuario_Rol`, `Id_Usuario`, `Id_Rol`, `Estado`) 
+VALUES ('1', '1', '1',  1);
 
 /* 
  * INICIA ROLLER 

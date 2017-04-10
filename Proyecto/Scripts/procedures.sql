f120ecee20d4610c07f77e701e5e44dbddb187e5
jean saborio <jecasau14@gmail.com>
2017-04-09 23:12:09 -0600

    sp
    sp

--- Proyecto/Scripts/procedures.sql
+++ Proyecto/Scripts/procedures.sql
@@ -38,18 +38,22 @@ VALUES ('1', 'Osvaldo', 'Aguero', 'Perez', '1', '1', 'Costa Rica', '2017-01-01',
 INSERT INTO `bd_elearning`.`tb_usuario` (`Id_Usuario`, `Nombre`, `Primer_Apellido`, `Segundo_Apellido`, `Clave`, `Id_Genero`, `Pais`, `Fecha_Ultimo_Ingreso`, `IP`, `SO`, `Navegador`, `Lenguaje`) 
 VALUES ('2', 'Roller', 'Zuniga', 'Solano', '2', '1', 'Costa Rica', '2017-01-01', '1.1.1.1', 'Windows', 'Explorer', 'L1');
 INSERT INTO `bd_elearning`.`tb_usuario` (`Id_Usuario`, `Nombre`, `Primer_Apellido`, `Segundo_Apellido`, `Clave`, `Id_Genero`, `Pais`, `Fecha_Ultimo_Ingreso`, `IP`, `SO`, `Navegador`, `Lenguaje`) 
-VALUES ('3', 'Janca', 'Saborio', 'Ugalde', '3', '1', 'Costa Rica', '2017-01-01', '1.1.1.1', 'Windows', 'Explorer', 'L1');
+VALUES ('3', 'Jeanca', 'Saborio', 'Ugalde', '3', '1', 'Costa Rica', '2017-01-01', '1.1.1.1', 'Windows', 'Explorer', 'L1');
 INSERT INTO `bd_elearning`.`tb_usuario` (`Id_Usuario`, `Nombre`, `Primer_Apellido`, `Segundo_Apellido`, `Clave`, `Id_Genero`, `Pais`, `Fecha_Ultimo_Ingreso`, `IP`, `SO`, `Navegador`, `Lenguaje`) 
 VALUES ('4', 'Antonio', 'Solis', 'Solis', '4', '1', 'Costa Rica', '2017-01-01', '1.1.1.1', 'Windows', 'Explorer', 'L1');
+INSERT INTO `bd_elearning`.`tb_usuario` (`Id_Usuario`, `Nombre`, `Primer_Apellido`, `Segundo_Apellido`, `Clave`, `Id_Genero`, `Pais`, `Fecha_Ultimo_Ingreso`, `IP`, `SO`, `Navegador`, `Lenguaje`) 
+VALUES ('5', 'Fernando', 'Chacon', 'Chacon', '5', '1', 'Costa Rica', '2017-01-01', '1.1.1.1', 'Windows', 'Explorer', 'L1');
 
 INSERT INTO `bd_elearning`.`tb_usuario_rol` (`Id_Usuario_Rol`, `Id_Usuario`, `Id_Rol`, `Estado`) 
 VALUES ('1', '1', '1',  1);
 INSERT INTO `bd_elearning`.`tb_usuario_rol` (`Id_Usuario_Rol`, `Id_Usuario`, `Id_Rol`, `Estado`) 
-VALUES ('2', '2', '5',  1);
+VALUES ('2', '2', '2',  1);
 INSERT INTO `bd_elearning`.`tb_usuario_rol` (`Id_Usuario_Rol`, `Id_Usuario`, `Id_Rol`, `Estado`) 
-VALUES ('3', '3', '1',  1);
+VALUES ('3', '3', '3',  1);
 INSERT INTO `bd_elearning`.`tb_usuario_rol` (`Id_Usuario_Rol`, `Id_Usuario`, `Id_Rol`, `Estado`) 
-VALUES ('4', '4', '5',  1);
+VALUES ('4', '4', '4',  1);
+INSERT INTO `bd_elearning`.`tb_usuario_rol` (`Id_Usuario_Rol`, `Id_Usuario`, `Id_Rol`, `Estado`) 
+VALUES ('5', '5', '5',  1);
 
 INSERT INTO `bd_elearning`.`tb_curso_usuario` (`Id_Curso_Usuario`,`Id_Curso`, `Id_Usuario`) VALUES ('1','1', '2');
 

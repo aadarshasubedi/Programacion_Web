83c07496d8a0873523270d05a11ed7a98a820f7b
Osvaldo <osv_93@hotmail.es>
2017-05-06 14:23:55 -0600

    Arreglo del error en el top del jsPrincipal

--- Proyecto/E-Learning/resourses/js/jsPrincipal.js
+++ Proyecto/E-Learning/resourses/js/jsPrincipal.js
@@ -10,24 +10,28 @@ $(document).ready(function(){
 
     // Using jQuery's animate() method to add smooth page scroll
     // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function(){
-   
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-  });
+    if (hash.length){
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+     
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });  
   
   $(window).scroll(function() {
+    if ($(".slideanim").length){
     $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
+      var pos = $(".slideanim").offset().top;
 
       var winTop = $(window).scrollTop();
         if (pos < winTop + 600) {
           $(this).addClass("slide");
         }
     });
+  }
   });
 })
 

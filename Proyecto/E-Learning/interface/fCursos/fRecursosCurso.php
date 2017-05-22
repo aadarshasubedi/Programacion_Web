f2741515f44e50360d41cb2ecc08a5dec06ec313
Osvaldo <osv_93@hotmail.es>
2017-05-21 19:22:05 -0600

    implementacion de audio

--- Proyecto/E-Learning/interface/fCursos/fRecursosCurso.php
+++ Proyecto/E-Learning/interface/fCursos/fRecursosCurso.php
@@ -214,3 +214,23 @@
 
 	</div>
 </div>
+
+<div id="modalAudio" class="modal fade" data-backdrop="static" role="dialog">
+	<div class="modal-dialog">
+
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 id="title" class="modal-title"></h4>
+			</div>
+			<div class="modal-body">
+				<audio controls>
+				  <source id="audio" src="" type="" >
+					El navegador no soporta este tipo de audio
+				</audio>			
+			</div>
+		</div>
+
+	</div>
+</div>
